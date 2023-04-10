0360e31e12db79f95e7e9b6bf67281cf98f7596c
Khadicha <246abdurashidova123@gmail.com>
2023-04-10 11:48:30 +0500

    sttle finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Header from "./layouts/Header";
+import Main from "./components/Main";
+import Footer from "./layouts/Footer";
 
 import "./assets/styles/Header.css";
+import "./assets/styles/Main.css";
+import "./assets/styles/Footer.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,2 +1,25 @@
 // import logo from './logo.svg';
 // <img src={logo} className="App-logo" alt="logo" />
+
+import image from "../assets/images/image.svg.jpg";
+import React from "react";
+
+function Main() {
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="smallContainer">
+          <div className="left">
+            <span>All:</span> 99
+          </div>
+          <div className="right">
+            <div>0</div> / <div>99</div>
+          </div>
+        </div>
+        <div className="circle">99</div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
